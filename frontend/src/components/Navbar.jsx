605a75c37840b28a7731637771e79333ee11ebd0
I-amPrashant <airmax50cent@gmail.com>
2025-06-23 07:51:44 +0545

    navbar component add

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import {NavLink } from 'react-router-dom';
+import {useSelector} from 'react-redux'
+import {Link} from 'react-router-dom'
+import logo from '../assets/logo.svg'
+import hamburger from '../assets/hamburger.svg'
+import cross from '../assets/cross.svg'
 
 export default function Navbar() {
+  const {currentUser} =useSelector(state=>state.user);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth>1024);
+
+  const handleResize = () => {
+    if(window.innerWidth>1024){
+      return setSidebarOpen(false);
+    }
+    setSidebarOpen(true);
+  }
+  
+  useEffect(() => {
+    handleResize();
+
+    window.addEventListener("resize",handleResize);
+
+    return () => window.removeEventListener("resize",handleResize);
+  }, []);
+
   return (
-    <div>Navbar</div>
+    <nav className='px-10 py-6 flex justify-between items-center font-nav shadow-md'>
+
+          {/* logo wrapper */}
+          <div>
+              <img src={logo} alt="logo" className='w-[130px]'/>
+          </div>
+
+          {/* nav links wrapper */}
+          <div className={`absolute z-[150] top-0 right-0 min-w-[200px] w-max h-full border-l border-gray-500 lg:border-none lg:h-fit lg:relative flex-center transition-default ${sidebarOpen? 'sidebar-open' : ''} `}>
+              <ul className='bg-white flex flex-col gap-8 justify-start items-start h-full pt-[125px] pl-10 pr-24 fixed lg:px-0 lg:pt-0 lg:relative lg:h-fit lg:flex-row lg:gap-8 lg:items-center'>
+                {/* to manage active link either use LInk with location hook or just use NavLink from react router  */}
+                    <li className='nav-link'><NavLink className={({isActive})=> isActive ? 'active' : ''} to='/' onClick={() => !isLargeScreen &&  setSidebarOpen(!sidebarOpen)}>Home</NavLink></li>
+                    <li className='nav-link'><NavLink className={({isActive})=> isActive ? 'active' : ''} to='/find-job' onClick={() => !isLargeScreen && setSidebarOpen(!sidebarOpen)}>Find Job</NavLink></li>
+                    <li className='nav-link'><NavLink className={({isActive})=> isActive ? 'active' : ''} to='/resources' onClick={() => !isLargeScreen && setSidebarOpen(!sidebarOpen)}>Resources</NavLink></li>
+                    <li className='nav-link'><NavLink className={({isActive})=> isActive ? 'active' : ''} to='/about' onClick={() => !isLargeScreen && setSidebarOpen(!sidebarOpen)}>About Us</NavLink></li>
+                    <li className='nav-link'><NavLink className={({isActive})=> isActive ? 'active' : ''} to='/contact' onClick={() => !isLargeScreen && setSidebarOpen(!sidebarOpen)}>Contact Us</NavLink></li>
+
+                    {currentUser? (
+                      <button className='text-accent-secondary underline underline-offset-2 -mt-1'>Log out</button>
+                    ):(<button className='btn-primary'>
+                      <Link to='/auth/sign-up'>Sign Up</Link>
+                    </button>)}
+
+                    <span className='block absolute top-5 right-5 lg:hidden'><img src={cross} alt="cross-icon" className='w-8 cursor-pointer' onClick={() => setSidebarOpen(!sidebarOpen)} id='cross'/></span>
+              </ul>
+          </div>
+
+          {/* hambuger  */}
+          <div className='block lg:hidden'>
+              <img src={hamburger} alt="hamburger" className='w-8 cursor-pointer' onClick={() => setSidebarOpen(!sidebarOpen)} id='hamburger'/>
+          </div>
+    </nav>
   )
 }
