116910d508f47e7ac66ae39945dc9946fbef3a5a
I-amPrashant <airmax50cent@gmail.com>
2025-06-23 16:35:07 +0545

    slight UI added

--- frontend/src/pages/FIndjob.jsx
+++ frontend/src/pages/FIndjob.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 import resume from "../assets/resume.svg";
 import webDev from "../assets/webDev.svg";
 import bookmark from "../assets/bookmark.svg";
@@ -10,20 +10,19 @@ export default function FIndjob() {
   const [loading, setLoading] = useState(false);
   const fileRef = useRef(null);
 
-  const handleFileSelect=async()=>{
+  const handleFileSelect = async () => {
     fileRef.current.click();
-  }
+  };
 
-  const handleFileChange=async(e)=>{
-    const selectedFile=e.target.files[0];
-    if(selectedFile){
+  const handleFileChange = async (e) => {
+    const selectedFile = e.target.files[0];
+    if (selectedFile) {
       setFile(selectedFile);
     }
-  }
+  };
   const handleUpload = async () => {
     if (!file) {
       return toast.error("Please select a file");
-      
     }
     try {
       setLoading(true);
@@ -36,156 +35,201 @@ export default function FIndjob() {
   };
 
   return (
-    <section className="section-css max-w-[1000px]">
+    <section className="section-css max-w-[1500px]">
       <h2 className="font-body text-start font-semibold">
         Find your ideal job
       </h2>
 
+      <div className="grid grid-cols-1 gap-y-[100px] lg:gap-y-0 lg:grid-cols-3 gap-14">
+        <div className="lg:col-span-2">
+          <div
+            className="relative px-4 py-5 flex items-center gap-5 border-2 bg-accent-secondary/5  border-accent-secondary hover:bg-accent-secondary/20 transition-default rounded-xl cursor-pointer"
+            onClick={handleFileSelect}
+          >
+            {loading ? (
+              <div className="flex-center w-full">
+                <img
+                  src={loadingIcon}
+                  alt="loading-icon"
+                  className="w-12 h-12"
+                />
+              </div>
+            ) : (
+              <>
+                <input
+                  type="file"
+                  className="hidden"
+                  onChange={(e) => handleFileChange(e)}
+                  ref={fileRef}
+                  accept="application/pdf"
+                />
+                <div>
+                  <img src={resume} alt="resume-icon" className="w-8 h-8" />
+                </div>
+
+                <div className="flex flex-col gap-1 text-start">
+                  <p className="font-nav font-semibold text-xl">
+                    Upload your resume
+                  </p>
+                  <p className="font-body text-shadow-accent-secondary">
+                    We'll match you with your ideal career Right now, Right
+                    away!
+                  </p>
+                  <span className="absolute right-5 top-1/2 -translate-y-1/2 text-lg">
+                    {file?.name ? "✔" : "+"}
+                  </span>
+                </div>
+              </>
+            )}
+          </div>
+
+          <button className="btn-secondary mx-auto mt-5 " onClick={handleUpload}>
+            Upload
+          </button>
+
+          {/* recommended job roles  */}
+          <div className="flex flex-col gap-10 mt-5">
+            <div className="flex justify-between">
+              <p className="font-body text-textColor-secondary font-medium">
+                284 results found
+              </p>
+
+              {/* filter feature */}
+            </div>
+
+            {/* job roles  */}
+            <div className="flex flex-col gap-6 w-full relative">
+              {/* job items  */}
+              <div className="flex gap-4 px-4 py-6 shadow-md">
+                {/* image  */}
+                <div className="flex-center p-2 h-fit border-[1px] border-gray-200">
+                  <img
+                    src={webDev}
+                    alt="job-icon"
+                    className="min-w-15 h-15 object-cover"
+                  />
+                </div>
+                {/* content  */}
+                <div className="flex grow text-start flex-col gap-1">
+                  <h6 className="font-nav font-semibold">Web Developer</h6>
+                  <p className="font-body font-medium text-textColor-secondary max-w-[600px]">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Consequatur odio in, veritatis maiores itaque harum incidunt
+                    esse ill.
+                  </p>
+                  {/* roadmap  */}
+                  <div className="mt-3">
+                    <button className="btn-secondary">Roadmap</button>
+                  </div>
+
+                  <div className="flex justify-between items-center flex-wrap mt-5">
+                    {/* salary  */}
+                    <div className="flex flex-col gap-1 font-medium">
+                      <p className="text-textColor-secondary">Avg. Salary</p>
+                      <p className="text-shadow-textColor-primary">
+                        $30,000 - $50,000
+                      </p>
+                    </div>
+
+                    {/* save job  */}
+                    <div className="inline">
+                      <button className="flex">
+                        <img
+                          src={bookmark}
+                          alt="bookmarkIcon"
+                          className="w-6 h-6 opacity-35"
+                        />
+                        <span className="ml-2">Save job</span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* job items  */}
+              <div className="flex gap-4 px-4 py-6 shadow-md">
+                {/* image  */}
+                <div className="flex-center p-2 h-fit border-[1px] border-gray-200">
+                  <img
+                    src={webDev}
+                    alt="job-icon"
+                    className="min-w-15 h-15 object-cover"
+                  />
+                </div>
+                {/* content  */}
+                <div className="flex grow text-start flex-col gap-1">
+                  <h6 className="font-nav font-semibold">Web Developer</h6>
+                  <p className="font-body font-medium text-textColor-secondary max-w-[600px]">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Consequatur odio in, veritatis maiores itaque harum incidunt
+                    esse ill.
+                  </p>
+                  {/* roadmap  */}
+                  <div className="mt-3">
+                    <button className="btn-secondary">Roadmap</button>
+                  </div>
+
+                  <div className="flex justify-between items-center flex-wrap mt-5">
+                    {/* salary  */}
+                    <div className="flex flex-col gap-1 font-medium">
+                      <p className="text-textColor-secondary">Avg. Salary</p>
+                      <p className="text-shadow-textColor-primary">
+                        $30,000 - $50,000
+                      </p>
+                    </div>
+
+                    {/* save job  */}
+                    <div className="inline">
+                      <button className="flex">
+                        <img
+                          src={bookmark}
+                          alt="bookmarkIcon"
+                          className="w-6 h-6 opacity-35"
+                        />
+                        <span className="ml-2">Save job</span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col lg:col-span-1 gap-5">
+              <h6 className="font-nav font-semibold text-start">Recommended for you</h6>
+              <div className="flex flex-col gap-5">
+
+                  {/* recommended job UI  */}
+                  <div className="flex gap-3 items-center">
+                    <img src="https://plus.unsplash.com/premium_photo-1750063400799-d3d386a86c36?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8" alt="" className="w-16 h-16 object-cover" />
+
+                    <div className="flex text-start flex-col gap-1">
+                        <p className="font-nav font-medium">Web Developer</p>
+                        <p className="font-body text-textColor-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                  </div>
+                  {/* recommended job UI  */}
+                  <div className="flex gap-3 items-center">
+                    <img src="https://images.unsplash.com/photo-1750008267598-7f68e1a25ab8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8" alt="" className="w-16 h-16 object-cover" />
+
+                    <div className="flex text-start flex-col gap-1">
+                        <p className="font-nav font-medium">Web Designer</p>
+                        <p className="font-body text-textColor-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                  </div>
+                  {/* recommended job UI  */}
+                  <div className="flex gap-3 items-center">
+                    <img src="https://images.unsplash.com/photo-1750101272034-7becde7454dd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw5fHx8ZW58MHx8fHx8" alt="" className="w-16 h-16 object-cover" />
+
+                    <div className="flex text-start flex-col gap-1">
+                        <p className="font-nav font-medium">Backend engineer</p>
+                        <p className="font-body text-textColor-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                  </div>
+              </div>
+        </div>
+      </div>
       {/* resume upload button  */}
-      <div
-        className="relative px-4 py-5 flex items-center gap-5 border-2 bg-accent-secondary/5  border-accent-secondary hover:bg-accent-secondary/20 transition-default rounded-xl cursor-pointer"
-        onClick={handleFileSelect}
-      >
-        {loading ? (
-          <div className="flex-center w-full">
-            <img src={loadingIcon} alt="loading-icon" className="w-12 h-12"/>
-          </div>
-        ) : (
-          <>
-            <input
-              type="file"
-              className="hidden"
-              onChange={(e)=>handleFileChange(e)}
-              ref={fileRef}
-              accept="application/pdf"
-            />
-            <div>
-              <img src={resume} alt="resume-icon" className="w-8 h-8" />
-            </div>
-
-            <div className="flex flex-col gap-1 text-start">
-              <p className="font-nav font-semibold text-xl">
-                Upload your resume
-              </p>
-              <p className="font-body text-shadow-accent-secondary">
-                We'll match you with your ideal career Right now, Right away!
-              </p>
-              <span className="absolute right-5 top-1/2 -translate-y-1/2 text-lg">
-                {file?.name?'✔':'+'}
-              </span>
-            </div>
-          </>
-        )}
-      </div>
-
-      <button className="btn-secondary mx-auto " onClick={handleUpload}>Upload</button>
-
-      {/* recommended job roles  */}
-      <div className="flex flex-col gap-10 mt-5">
-        <div className="flex justify-between">
-          <p className="font-body text-textColor-secondary font-medium">
-            284 results found
-          </p>
-
-          {/* filter feature */}
-        </div>
-
-        {/* job roles  */}
-        <div className="flex flex-col gap-6 w-full relative">
-          {/* job items  */}
-          <div className="flex gap-4 px-4 py-6 shadow-md">
-            {/* image  */}
-            <div className="flex-center p-2 h-fit border-[1px] border-gray-200">
-              <img
-                src={webDev}
-                alt="job-icon"
-                className="min-w-15 h-15 object-cover"
-              />
-            </div>
-            {/* content  */}
-            <div className="flex grow text-start flex-col gap-1">
-              <h6 className="font-nav font-semibold">Web Developer</h6>
-              <p className="font-body font-medium text-textColor-secondary max-w-[600px]">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Consequatur odio in, veritatis maiores itaque harum incidunt
-                esse ill.
-              </p>
-              {/* roadmap  */}
-              <div className="mt-3">
-                <button className="btn-secondary">Roadmap</button>
-              </div>
-
-              <div className="flex justify-between items-center flex-wrap mt-5">
-                {/* salary  */}
-                <div className="flex flex-col gap-1 font-medium">
-                  <p className="text-textColor-secondary">Avg. Salary</p>
-                  <p className="text-shadow-textColor-primary">
-                    $30,000 - $50,000
-                  </p>
-                </div>
-
-                {/* save job  */}
-                <div className="inline">
-                  <button className="flex">
-                    <img
-                      src={bookmark}
-                      alt="bookmarkIcon"
-                      className="w-6 h-6 opacity-35"
-                    />
-                    <span className="ml-2">Save job</span>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* job items  */}
-          <div className="flex gap-4 px-4 py-6 shadow-md">
-            {/* image  */}
-            <div className="flex-center p-2 h-fit border-[1px] border-gray-200">
-              <img
-                src={webDev}
-                alt="job-icon"
-                className="min-w-15 h-15 object-cover"
-              />
-            </div>
-            {/* content  */}
-            <div className="flex grow text-start flex-col gap-1">
-              <h6 className="font-nav font-semibold">Web Developer</h6>
-              <p className="font-body font-medium text-textColor-secondary max-w-[600px]">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Consequatur odio in, veritatis maiores itaque harum incidunt
-                esse ill.
-              </p>
-              {/* roadmap  */}
-              <div className="mt-3">
-                <button className="btn-secondary">Roadmap</button>
-              </div>
-
-              <div className="flex justify-between items-center flex-wrap mt-5">
-                {/* salary  */}
-                <div className="flex flex-col gap-1 font-medium">
-                  <p className="text-textColor-secondary">Avg. Salary</p>
-                  <p className="text-shadow-textColor-primary">
-                    $30,000 - $50,000
-                  </p>
-                </div>
-
-                {/* save job  */}
-                <div className="inline">
-                  <button className="flex">
-                    <img
-                      src={bookmark}
-                      alt="bookmarkIcon"
-                      className="w-6 h-6 opacity-35"
-                    />
-                    <span className="ml-2">Save job</span>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </section>
   );
 }
