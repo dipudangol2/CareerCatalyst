02ee5bcd24af51dca2cd0c7079e3161cc1b8b73f
dipudangol2 <dipudangol2@gmail.com>
2025-06-23 07:44:18 +0545

    feat:Server setup and authRoutes

---
+++ server/models/UserModel.js
@@ -0,0 +1,28 @@
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: [true, "Fullname is required!"],
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, "Email is required!"],
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required!"],
+    },
+});
+
+userSchema.pre("save", async function (next) {
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+const User = mongoose.model("Users", userSchema);
+
+export default User;
